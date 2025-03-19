b261d45c6de43b982ee4d25769bbe577b16e64a5
David Capilla <david.capilla@gmail.com>
2025-03-19 12:17:10 +0000

    DXT plugin integration

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,71 @@
-import path from 'path'
+import defraForms from '@defra/forms-engine-plugin'
+import crumb from '@hapi/crumb'
 import hapi from '@hapi/hapi'
-
+import inert from '@hapi/inert'
+import path from 'path'
 import { config } from '~/src/config/config.js'
 import { nunjucksConfig } from '~/src/config/nunjucks/nunjucks.js'
-import { router } from './router.js'
-import { requestLogger } from '~/src/server/common/helpers/logging/request-logger.js'
 import { catchAll } from '~/src/server/common/helpers/errors.js'
-import { secureContext } from '~/src/server/common/helpers/secure-context/index.js'
-import { sessionCache } from '~/src/server/common/helpers/session-cache/session-cache.js'
-import { getCacheEngine } from '~/src/server/common/helpers/session-cache/cache-engine.js'
+import { requestLogger } from '~/src/server/common/helpers/logging/request-logger.js'
+import { setupProxy } from '~/src/server/common/helpers/proxy/setup-proxy.js'
 import { pulse } from '~/src/server/common/helpers/pulse.js'
 import { requestTracing } from '~/src/server/common/helpers/request-tracing.js'
-import { setupProxy } from '~/src/server/common/helpers/proxy/setup-proxy.js'
+import { secureContext } from '~/src/server/common/helpers/secure-context/index.js'
+import { getCacheEngine } from '~/src/server/common/helpers/session-cache/cache-engine.js'
+import { sessionCache } from '~/src/server/common/helpers/session-cache/session-cache.js'
+import LandParcelController from '~/src/server/controllers/land-parcel.js'
+import landGrantsDefinition from '~/src/server/forms/find-funding-for-land-or-farms.json'
+import { router } from './router.js'
+
+// Form metadata
+const now = new Date()
+const user = {
+  id: 'grants-user',
+  displayName: 'Grants dev'
+}
+const author = {
+  createdAt: now,
+  createdBy: user,
+  updatedAt: now,
+  updatedBy: user
+}
+
+const metadata = {
+  organisation: 'Defra',
+  teamName: 'Grants',
+  teamEmail: 'grants@defra.gov.uk',
+  submissionGuidance: "Thanks for your submission, we'll be in touch",
+  notificationEmail: 'cl-defra-tactical-grants-test-rpa-email@equalexperts.com',
+  ...author,
+  live: author
+}
+
+const landGrantsMetadata = {
+  id: '5c67688f-3c61-4839-a6e1-d48b598257f1',
+  slug: 'find-funding-for-land-or-farms',
+  title: 'Find Funding for Land or Farms',
+  ...metadata
+}
+
+const formsService = {
+  getFormMetadata: function (slug) {
+    console.log('alo??', { slug })
+    switch (slug) {
+      case landGrantsMetadata.slug:
+        return Promise.resolve(landGrantsMetadata)
+      default:
+        throw Boom.notFound(`Form '${slug}' not found`)
+    }
+  },
+  getFormDefinition: function (id, _state) {
+    switch (id) {
+      case landGrantsMetadata.id:
+        return Promise.resolve(landGrantsDefinition)
+      default:
+        throw Boom.notFound(`Form '${id}' not found`)
+    }
+  }
+}
 
 export async function createServer() {
   setupProxy()
@@ -52,6 +106,32 @@ export async function createServer() {
       strictHeader: false
     }
   })
+
+  await server.register({
+    plugin: defraForms,
+    options: {
+      services: {
+        formsService
+      },
+      controllers: {
+        LandParcelController
+      }
+    }
+  })
+
+  // Defra Forms & dependencies
+  // await server.register(pino)
+  await server.register(inert)
+  await server.register(crumb)
+  // await server.register({
+  //   plugin: yar,
+  //   options: {
+  //     cookieOptions: {
+  //       password: config.get('session.cookie.password')
+  //     }
+  //   }
+  // })
+
   await server.register([
     requestLogger,
     requestTracing,
@@ -59,7 +139,7 @@ export async function createServer() {
     pulse,
     sessionCache,
     nunjucksConfig,
-    router // Register all the controllers/routes defined in src/server/router.js
+    router // Register all the controllers/routes defined in src/server/router.js,
   ])
 
   server.ext('onPreResponse', catchAll)
