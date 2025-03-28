7b80b70c7b9ce469c1653c12f72f73dad15cbe80
Alan Platt <alan.platt@equalexperts.com>
2025-03-28 16:07:16 +0000

    Use dummy submission for adding value

--- src/server/common/forms/services/form.js
+++ src/server/common/forms/services/form.js
@@ -1,6 +1,6 @@
 import Boom from '@hapi/boom'
 import fs from 'fs/promises'
-import { exampleGrantMetadata, landGrantsMetadata } from '../config.js'
+import { addingValueMetadata, exampleGrantMetadata, landGrantsMetadata } from '../config.js'
 
 const getJsonFromFile = async function (path) {
   const url = new URL(`../definitions/${path}`, import.meta.url).pathname
@@ -10,6 +10,8 @@ const getJsonFromFile = async function (path) {
 export const formsService = {
   getFormMetadata: async function (slug) {
     switch (slug) {
+      case addingValueMetadata.slug:
+        return Promise.resolve(addingValueMetadata)
       case exampleGrantMetadata.slug:
         return Promise.resolve(exampleGrantMetadata)
       case landGrantsMetadata.slug:
@@ -19,10 +21,13 @@ export const formsService = {
     }
   },
   getFormDefinition: async function (id) {
+    const addingValue = getJsonFromFile('adding-value.json')
     const exampleGrant = getJsonFromFile('example-grant.json')
     const landGrants = getJsonFromFile('find-funding-for-land-or-farms.json')
 
     switch (id) {
+      case addingValueMetadata.id:
+        return Promise.resolve(addingValue)
       case exampleGrantMetadata.id:
         return Promise.resolve(exampleGrant)
       case landGrantsMetadata.id:
