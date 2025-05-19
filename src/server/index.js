d7ed06514aed84ae3602219b8b9fd2c991db4114
David Capilla <david.capilla@gmail.com>
2025-05-19 15:49:42 +0100

    Revert some unrelated DefraID changes

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ import {
   grantsUiPaths,
   nunjucksConfig
 } from '~/src/config/nunjucks/nunjucks.js'
-import auth from '~/src/plugins/auth.js'
+// import auth from '~/src/plugins/auth.js'
 import csp from '~/src/plugins/content-security-policy.js'
 import sso from '~/src/plugins/sso.js'
 import { formsService } from '~/src/server/common/forms/services/form.js'
@@ -132,7 +132,7 @@ const registerPlugins = async (server) => {
     Cookie,
     Scooter,
     csp,
-    auth,
+    // auth,
     requestLogger,
     requestTracing,
     secureContext,
