f2781ff3ba92c927ee1b289652b76e713d6a5f95
Andrew Folga <andrew.folga@equalexperts.com>
2025-06-12 14:13:07 +0100

    Code cleanup - auth config moved to grant config yaml

--- src/server/common/forms/config.js
+++ src/server/common/forms/config.js
@@ -19,27 +19,3 @@ export const metadata = {
   ...author,
   live: author
 }
-
-export const addingValueMetadata = {
-  id: '95e92559-968d-44ae-8666-2b1ad3dffd31',
-  slug: 'adding-value',
-  title: 'Adding value',
-  authRequired: true,
-  ...metadata
-}
-
-export const exampleGrantMetadata = {
-  id: '5eeb9f71-44f8-46ed-9412-3d5e2c5ab2bc',
-  slug: 'example-grant',
-  title: 'Example grant',
-  authRequired: false,
-  ...metadata
-}
-
-export const landGrantsMetadata = {
-  id: '5c67688f-3c61-4839-a6e1-d48b598257f1',
-  slug: 'find-funding-for-land-or-farms',
-  title: 'Find Funding for Land or Farms',
-  authRequired: true,
-  ...metadata
-}
