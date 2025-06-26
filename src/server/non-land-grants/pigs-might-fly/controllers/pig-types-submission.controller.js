bc172df10978a16f7b515b8988526de84dd22713
Tej Powar <tej@powtech.co.uk>
2025-06-26 16:42:32 +0100

    update controller and mapping for flying pigs

---
+++ src/server/non-land-grants/pigs-might-fly/controllers/pig-types-submission.controller.js
@@ -0,0 +1,67 @@
+import { SummaryPageController } from '@defra/forms-engine-plugin/controllers/SummaryPageController.js'
+
+import { transformStateObjectToGasApplication } from '~/src/server/common/helpers/grant-application-service/state-to-gas-payload-mapper.js'
+import { stateToPigsMightFlyGasAnswers } from '~/src/server/non-land-grants/pigs-might-fly/mappers/state-to-gas-pigs-mapper.js'
+import { submitGrantApplication } from '~/src/server/common/services/grant-application/grant-application.service.js'
+import { getFormsCacheService } from '~/src/server/common/helpers/forms-cache/forms-cache.js'
+
+export default class FlyingPigsSubmissionPageController extends SummaryPageController {
+  /**
+   * @param {FormModel} model
+   * @param {PageSummary} pageDef
+   */
+  constructor(model, pageDef) {
+    super(model, pageDef)
+    this.grantCode = this.model.def.metadata.submission.grantCode
+    this.viewName = 'submission'
+  }
+
+  /**
+   * Gets the path to the status page (in this case /confirmation page) for the POST handler.
+   * @returns {string} path to the status page
+   */
+  getStatusPath() {
+    return '/find-funding-for-land-or-farms/confirmation'
+  }
+
+  async submitPigTypesApplication(context) {
+    const {
+      sbi = 'sbi',
+      crn = 'crn',
+      defraId = 'defraId',
+      frn = 'frn'
+    } = context.state
+    const identifiers = {
+      sbi,
+      frn,
+      crn,
+      defraId,
+      clientRef: context.referenceNumber?.toLowerCase()
+    }
+    const applicationData = transformStateObjectToGasApplication(
+      identifiers,
+      context.state,
+      stateToPigsMightFlyGasAnswers
+    )
+
+    return submitGrantApplication(this.grantCode, applicationData)
+  }
+
+  makePostRouteHandler() {
+    const fn = async (request, context, h) => {
+      const result = await this.submitPigTypesApplication(context)
+
+      request.logger.info('Form submission completed', result)
+      const cacheService = getFormsCacheService(request.server)
+      await cacheService.setConfirmationState(request, { confirmed: true })
+      return h.redirect('/state')
+    }
+
+    return fn
+  }
+}
+
+/**
+ * @import { type FormModel } from '~/src/server/plugins/engine/models/index.js'
+ * @import { type PageSummary } from '@defra/forms-model'
+ */
