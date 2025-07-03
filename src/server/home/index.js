2df91b8a7be9798ea12fc5f2a6078c9b7d0e2da8
Alan Platt <alan.platt@equalexperts.com>
2025-07-03 23:31:08 +0100

    refactor: home feature - restructure into feature-based folders

--- src/server/home/index.js
+++ src/server/home/index.js
@@ -1,7 +1,4 @@
-import {
-  homeController,
-  indexController
-} from '~/src/server/home/controller.js'
+import { homeController, indexController } from './home.controller.js'
 /**
  * Sets up the routes used in the home page.
  * These routes are registered in src/server/router.js.
