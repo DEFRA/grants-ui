6cdd09b221115f410dcd1419b6e04cf83eb548b7
Alan Platt <alan.platt@equalexperts.com>
2025-07-11 15:59:14 +0100

    refactor: remove About page and related tests

--- src/server/router.js
+++ src/server/router.js
@@ -1,6 +1,5 @@
 import inert from '@hapi/inert'
 import { config } from '~/src/config/config.js'
-import { about } from '~/src/server/about/index.js'
 import { auth } from '~/src/server/auth/index.js'
 import { serveStaticFiles } from '~/src/server/common/helpers/serve-static-files.js'
 import { health } from '~/src/server/health/index.js'
@@ -32,7 +31,7 @@ export const router = {
       await server.register([auth])
 
       // Application specific routes, add your own routes here
-      await server.register([home, about, agreements])
+      await server.register([home, agreements])
 
       // Generic tasklist routes
       await server.register([createTasklistRoute('example')])
