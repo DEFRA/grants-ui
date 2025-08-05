b2bcc83735810276eebf8641eb1fba5e49f62380
David Middleton <david.middleton@esynergy.co.uk>
2025-08-05 11:31:56 +0100

    TGC-772: Add extra logging

--- src/server/common/helpers/state/get-cache-key-helper.js
+++ src/server/common/helpers/state/get-cache-key-helper.js
@@ -1,24 +1,95 @@
+import { log } from '~/src/server/common/helpers/logging/log.js'
+import { LogCodes } from '~/src/server/common/helpers/logging/log-codes.js'
+
 export const getCacheKey = (request) => {
   const credentials = request.auth?.credentials
 
   if (!credentials) {
-    throw new Error('Missing auth credentials')
+    log(LogCodes.AUTH.AUTH_DEBUG, {
+      path: 'getCacheKey',
+      isAuthenticated: request.auth?.isAuthenticated,
+      strategy: request.auth?.strategy,
+      mode: request.auth?.mode,
+      hasCredentials: !!credentials,
+      hasToken: false,
+      hasProfile: false,
+      userAgent: 'server',
+      referer: 'none',
+      queryParams: {},
+      authError: `Missing auth credentials ${JSON.stringify(credentials) || 'none'}`,
+      errorDetails: {
+        message: `Missing auth credentials ${JSON.stringify(credentials) || 'none'}`,
+        stack: null
+      }
+    })
+    throw new Error('1Missing auth credentials')
   }
   const { id: userId, relationships } = credentials
 
   if (!userId) {
-    throw new Error('Missing user ID in credentials')
+    log(LogCodes.AUTH.AUTH_DEBUG, {
+      path: 'getCacheKey',
+      isAuthenticated: request.auth?.isAuthenticated,
+      strategy: request.auth?.strategy,
+      mode: request.auth?.mode,
+      hasCredentials: !!credentials,
+      hasToken: false,
+      hasProfile: false,
+      userAgent: 'server',
+      referer: 'none',
+      queryParams: {},
+      authError: `Missing user ID in credentials ${JSON.stringify(credentials) || 'none'}`,
+      errorDetails: {
+        message: `Missing user ID in credentials ${JSON.stringify(credentials) || 'none'}`,
+        stack: null
+      }
+    })
+    throw new Error('2Missing user ID in credentials')
   }
 
   // Support single-business users for now
   const businessId = (Array.isArray(relationships) && relationships[0]?.split(':')[1]) || null
   if (!businessId) {
-    throw new Error(`Missing or malformed business relationship in credentials: ${JSON.stringify(relationships)}`)
+    log(LogCodes.AUTH.AUTH_DEBUG, {
+      path: 'getCacheKey',
+      isAuthenticated: request.auth?.isAuthenticated,
+      strategy: request.auth?.strategy,
+      mode: request.auth?.mode,
+      hasCredentials: !!credentials,
+      hasToken: false,
+      hasProfile: false,
+      userAgent: 'server',
+      referer: 'none',
+      queryParams: {},
+      authError: `Missing or malformed business relationship in credentials ${JSON.stringify(credentials) || 'none'}`,
+      errorDetails: {
+        message: `Missing or malformed business relationship in credentials ${JSON.stringify(credentials) || 'none'}`,
+        stack: null
+      }
+    })
+    throw new Error(`3Missing or malformed business relationship in credentials: ${JSON.stringify(relationships)}`)
   }
 
   const grantId = request.params?.slug
   if (!grantId) {
-    throw new Error('Missing grantId')
+    log(LogCodes.AUTH.AUTH_DEBUG, {
+      path: 'getCacheKey',
+      isAuthenticated: request.auth?.isAuthenticated,
+      strategy: request.auth?.strategy,
+      mode: request.auth?.mode,
+      hasCredentials: !!credentials,
+      hasToken: false,
+      hasProfile: false,
+      userAgent: 'server',
+      referer: 'none',
+      queryParams: {},
+      authError: `Missing grantId from params ${request.params}`,
+      errorDetails: {
+        message: `Missing grantId from params ${request.params}`,
+        stack: null
+      }
+    })
+    throw new Error('4Missing grantId')
   }
   return { userId, businessId, grantId }
 }
