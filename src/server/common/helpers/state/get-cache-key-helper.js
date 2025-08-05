c4b5bed20b2e447663633d993edc0c4f41f2b200
David Middleton <david.middleton@esynergy.co.uk>
2025-08-05 11:49:00 +0100

    TGC-772: Add extra logging

--- src/server/common/helpers/state/get-cache-key-helper.js
+++ src/server/common/helpers/state/get-cache-key-helper.js
@@ -1,94 +1,43 @@
 import { log } from '~/src/server/common/helpers/logging/log.js'
 import { LogCodes } from '~/src/server/common/helpers/logging/log-codes.js'
 
+const outputLog = (request, message) => {
+  log(LogCodes.AUTH.AUTH_DEBUG, {
+    path: 'getCacheKey',
+    isAuthenticated: request.auth?.isAuthenticated,
+    strategy: request.auth?.strategy,
+    mode: request.auth?.mode,
+    hasCredentials: false,
+    queryParams: {},
+    authError: `${message} ${JSON.stringify(request.auth?.credentials) || 'none'}`,
+    errorDetails: {}
+  })
+}
+
 export const getCacheKey = (request) => {
   const credentials = request.auth?.credentials
 
   if (!credentials) {
-    log(LogCodes.AUTH.AUTH_DEBUG, {
-      path: 'getCacheKey',
-      isAuthenticated: request.auth?.isAuthenticated,
-      strategy: request.auth?.strategy,
-      mode: request.auth?.mode,
-      hasCredentials: !!credentials,
-      hasToken: false,
-      hasProfile: false,
-      userAgent: 'server',
-      referer: 'none',
-      queryParams: {},
-      authError: `Missing auth credentials ${JSON.stringify(credentials) || 'none'}`,
-      errorDetails: {
-        message: `Missing auth credentials ${JSON.stringify(credentials) || 'none'}`,
-        stack: null
-      }
-    })
+    outputLog(request, 'Missing auth credentials')
     throw new Error('1Missing auth credentials')
   }
   const { id: userId, relationships } = credentials
 
   if (!userId) {
-    log(LogCodes.AUTH.AUTH_DEBUG, {
-      path: 'getCacheKey',
-      isAuthenticated: request.auth?.isAuthenticated,
-      strategy: request.auth?.strategy,
-      mode: request.auth?.mode,
-      hasCredentials: !!credentials,
-      hasToken: false,
-      hasProfile: false,
-      userAgent: 'server',
-      referer: 'none',
-      queryParams: {},
-      authError: `Missing user ID in credentials ${JSON.stringify(credentials) || 'none'}`,
-      errorDetails: {
-        message: `Missing user ID in credentials ${JSON.stringify(credentials) || 'none'}`,
-        stack: null
-      }
-    })
+    outputLog(request, 'Missing user ID in credentials')
     throw new Error('2Missing user ID in credentials')
   }
 
   // Support single-business users for now
   const businessId = (Array.isArray(relationships) && relationships[0]?.split(':')[1]) || null
   if (!businessId) {
-    log(LogCodes.AUTH.AUTH_DEBUG, {
-      path: 'getCacheKey',
-      isAuthenticated: request.auth?.isAuthenticated,
-      strategy: request.auth?.strategy,
-      mode: request.auth?.mode,
-      hasCredentials: !!credentials,
-      hasToken: false,
-      hasProfile: false,
-      userAgent: 'server',
-      referer: 'none',
-      queryParams: {},
-      authError: `Missing or malformed business relationship in credentials ${JSON.stringify(credentials) || 'none'}`,
-      errorDetails: {
-        message: `Missing or malformed business relationship in credentials ${JSON.stringify(credentials) || 'none'}`,
-        stack: null
-      }
-    })
+    outputLog(request, 'Missing or malformed business relationship in credentials')
     throw new Error(`3Missing or malformed business relationship in credentials: ${JSON.stringify(relationships)}`)
   }
 
   const grantId = request.params?.slug
   if (!grantId) {
-    log(LogCodes.AUTH.AUTH_DEBUG, {
-      path: 'getCacheKey',
-      isAuthenticated: request.auth?.isAuthenticated,
-      strategy: request.auth?.strategy,
-      mode: request.auth?.mode,
-      hasCredentials: !!credentials,
-      hasToken: false,
-      hasProfile: false,
-      userAgent: 'server',
-      referer: 'none',
-      queryParams: {},
-      authError: `Missing grantId from params ${request.params}`,
-      errorDetails: {
-        message: `Missing grantId from params ${request.params}`,
-        stack: null
-      }
-    })
+    outputLog(request, 'Missing grantId')
     throw new Error('4Missing grantId')
   }
   return { userId, businessId, grantId }
