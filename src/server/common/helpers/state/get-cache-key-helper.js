29a4acf31731ad1e4d0f0b28a782f793edc57ab1
David Middleton <david.middleton@esynergy.co.uk>
2025-08-05 15:12:58 +0100

    TGC-772: Revert extra logging Improve error messages with unique codes to work around log redaction

--- src/server/common/helpers/state/get-cache-key-helper.js
+++ src/server/common/helpers/state/get-cache-key-helper.js
@@ -1,44 +1,26 @@
-import { log } from '~/src/server/common/helpers/logging/log.js'
-import { LogCodes } from '~/src/server/common/helpers/logging/log-codes.js'
-
-const outputLog = (request, message) => {
-  log(LogCodes.AUTH.AUTH_DEBUG, {
-    path: 'getCacheKey',
-    isAuthenticated: request.auth?.isAuthenticated,
-    strategy: request.auth?.strategy,
-    mode: request.auth?.mode,
-    hasCredentials: false,
-    queryParams: {},
-    authError: `${message} ${JSON.stringify(request.auth?.credentials) || 'none'}`,
-    errorDetails: {}
-  })
-}
-
 export const getCacheKey = (request) => {
   const credentials = request.auth?.credentials
 
   if (!credentials) {
-    outputLog(request, 'Missing auth credentials')
-    throw new Error('1Missing auth credentials')
+    throw new Error('GCK001: Missing auth credentials')
   }
   const { crn: userId, relationships } = credentials
 
   if (!userId) {
-    outputLog(request, 'Missing user ID in credentials')
-    throw new Error('2Missing user ID in credentials')
+    throw new Error('GCK002: Missing user ID in credentials')
   }
 
   // Support single-business users for now
   const businessId = (Array.isArray(relationships) && relationships[0]?.split(':')[1]) || null
   if (!businessId) {
-    outputLog(request, 'Missing or malformed business relationship in credentials')
-    throw new Error(`3Missing or malformed business relationship in credentials: ${JSON.stringify(relationships)}`)
+    throw new Error(
+      `GCK003: Missing or malformed business relationship in credentials: ${JSON.stringify(relationships)}`
+    )
   }
 
   const grantId = request.params?.slug
   if (!grantId) {
-    outputLog(request, 'Missing grantId')
-    throw new Error('4Missing grantId')
+    throw new Error('GCK004: Missing grantId')
   }
   return { userId, businessId, grantId }
 }
