6af5fdd36ff37e6d107d0650052bfb1c73b5443e
Alistair Laing <aliuk2012@users.noreply.github.com>
2025-09-02 16:25:45 +0100

    fix users crn number

--- src/server/index.js
+++ src/server/index.js
@@ -197,14 +197,14 @@ const mockSessionData = async (request, log, LogCodes) => {
     const sessionData = {
       isAuthenticated: true,
       sessionId,
-      contactId: `987654321`,
+      contactId: config.get('landGrants.customerReferenceNumber'),
       firstName: 'Anonymous',
       lastName: 'User',
       name: 'Anonymous User',
       role: 'user',
       scope: ['user'],
       currentRelationshipId: `${sbiStore.get('sbi')}1234`,
-      crn: '987654321',
+      crn: config.get('landGrants.customerReferenceNumber'),
       relationships: [`${sbiStore.get('sbi')}1234:${sbiStore.get('sbi')}:Farm ${sbiStore.get('sbi')}`]
     }
 
