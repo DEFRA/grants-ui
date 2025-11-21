efd861d3ce4d8ae0ee6c13b9d2cca6d371da3d85
Alistair Laing <aliuk2012@users.noreply.github.com>
2025-11-21 15:05:57 +0000

    Add placeholder page for T&C

--- src/server/index.js
+++ src/server/index.js
@@ -35,6 +35,7 @@ import LandActionsCheckPageController from '~/src/server/land-grants/controllers
 import SelectLandParcelPageController from '~/src/server/land-grants/controllers/select-land-parcel-page.controller.js'
 import SelectLandActionsPageController from '~/src/server/land-grants/controllers/select-land-actions-page.controller.js'
 import SubmissionPageController from '~/src/server/land-grants/controllers/submission-page.controller.js'
+import TermsAndConditionsPageController from '~/src/server/land-grants/controllers/terms-and-conditions.controller.js'
 import FlyingPigsSubmissionPageController from '~/src/server/non-land-grants/pigs-might-fly/controllers/flying-pigs-submission-page.controller.js'
 import { PotentialFundingController } from '~/src/server/non-land-grants/pigs-might-fly/controllers/potential-funding.controller.js'
 import { tasklistBackButton } from '~/src/server/plugins/tasklist-back-button.js'
@@ -120,6 +121,7 @@ const registerFormsPlugin = async (server, prefix = '') => {
         ConfirmationPageController,
         DeclarationPageController,
         SubmissionPageController,
+        TermsAndConditionsPageController,
         ConfirmFarmDetailsController,
         SelectLandParcelPageController,
         SelectLandActionsPageController,
---
+++ src/server/land-grants/controllers/terms-and-conditions.controller.js
@@ -0,0 +1,10 @@
+import { QuestionPageController } from '@defra/forms-engine-plugin/controllers/QuestionPageController.js'
+
+export default class TermsAndConditionsPageController extends QuestionPageController {
+  viewName = 'terms-and-conditions'
+}
+
+/**
+ * @import { FormContext, AnyFormRequest } from '@defra/forms-engine-plugin/engine/types.js'
+ * @import { ResponseObject, ResponseToolkit } from '@hapi/hapi'
+ */
