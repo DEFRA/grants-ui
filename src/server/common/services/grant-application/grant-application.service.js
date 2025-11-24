0b5819807c3679be0a2f92d5d9603ba59aa1d555
David Capilla <david.capilla@gmail.com>
2025-11-24 13:27:46 +0000

    Testing GAS response logging

--- src/server/common/services/grant-application/grant-application.service.js
+++ src/server/common/services/grant-application/grant-application.service.js
@@ -1,5 +1,5 @@
 import { config } from '~/src/config/config.js'
-import { log, logger } from '~/src/server/common/helpers/logging/log.js'
+import { log } from '~/src/server/common/helpers/logging/log.js'
 import { retry } from '~/src/server/common/helpers/retry.js'
 
 const GAS_API_ENDPOINT = config.get('gas.apiEndpoint')
