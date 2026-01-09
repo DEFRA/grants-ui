afee7123ae084ffd2680e4173039d9cc7b5af251
Andrew Deacon <andrew.deacon@equalexperts.com>
2026-01-09 17:02:57 +0000

    Run land-grants-journey-tests in CI

--- tools/run-acceptance-tests.sh
+++ tools/run-acceptance-tests.sh
@@ -16,8 +16,9 @@ if [ -n "$FEATURE_FILE" ]; then
 fi
 
 export ACCEPTANCE_TESTS_HOOK="
-  docker compose -f gae-compose.yml run --quiet-pull --rm gae-acceptance-tests $TEST_COMMAND &&
-  docker compose -f gae-compose.yml down
+  docker compose -f acceptance-tests-compose.yml run --quiet-pull --rm gae-acceptance-tests $TEST_COMMAND &&
+  docker compose -f acceptance-tests-compose.yml run --quiet-pull --rm land-grants-journey-tests $TEST_COMMAND &&
+  docker compose -f acceptance-tests-compose.yml down
 "
 
 "$(dirname "$0")/docker-compose-smoke-test.sh"
