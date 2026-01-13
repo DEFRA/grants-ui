df18d32ff6e6e322d8e766b9a3af61a874076a14
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-01-13 16:20:02 +0000

    Add SSSI consent note to parcel hints when required

--- src/server/land-grants/view-models/parcel.view-model.js
+++ src/server/land-grants/view-models/parcel.view-model.js
@@ -31,6 +31,7 @@ export function buildParcelHint(parcel, actionsForParcel) {
   const size = parcel.size || parcel.area
   const hasArea = size?.value && formatAreaUnit(size.unit)
   const hasActions = actionsForParcel > 0
+  const needsSSSIConstent = parcel.SSSIRequired === true
 
   let hint = ''
   if (hasArea) {
@@ -40,6 +41,10 @@ export function buildParcelHint(parcel, actionsForParcel) {
   if (hasActions) {
     const actionsAddedStr = `${actionsForParcel} action${actionsForParcel > 1 ? 's' : ''} added`
     hint += hasArea ? `, ${actionsAddedStr}` : `${actionsAddedStr}`
+  }
+
+  if (needsSSSIConstent) {
+    hint += `, SSSI consent may be needed`
   }
 
   return hint
@@ -70,6 +75,7 @@ export function mapParcelsToViewModel(parcels, landParcels = {}) {
  * @property {object} [area] - Area information (fallback for backwards compatibility)
  * @property {string} [area.value] - Area value
  * @property {string} [area.unit] - Area unit
+ * @property {boolean} [SSSIRequired] - True if SSSI consent is required for the land parcel
  */
 
 /**
