af0cc02296a5f9f574d7b2369c0a5ae7328d958c
Andrew Folga <andrew.folga@equalexperts.com>
2026-04-07 11:12:53 +0100

    Disable performance tests temporarily

--- tools/run-all-tests.sh
+++ tools/run-all-tests.sh
@@ -16,9 +16,9 @@ export ACCEPTANCE_TESTS_HOOK="
   docker compose -f compose.tests.yml down
 "
 
-export PERFORMANCE_TESTS_HOOK="
-  docker compose -f compose.tests.yml run --quiet-pull --rm grants-ui-performance-tests &&
-  docker compose -f compose.tests.yml down
-"
+# export PERFORMANCE_TESTS_HOOK="
+#   docker compose -f compose.tests.yml run --quiet-pull --rm grants-ui-performance-tests &&
+#   docker compose -f compose.tests.yml down
+# "
 
 "$(dirname "$0")/docker-compose-smoke-test.sh"
