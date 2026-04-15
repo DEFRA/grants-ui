2a0a6ad28b3046cb6f3bdd3d0b8cc7461bc8044e
Andrew Deacon <82759990+ScenarioFactory@users.noreply.github.com>
2026-04-15 05:42:17 +0100

    Restore k6 perf testing in CI (#784)
    Restore k6 perf test running in CI

--- tools/run-performance-tests.sh
+++ tools/run-performance-tests.sh
@@ -5,8 +5,6 @@ set -e
 #   Run all performance tests:
 #     ./tools/run-performance-tests.sh
 
-"$(dirname "$0")/generate-users-csv.sh"
-
 export PERFORMANCE_TESTS_HOOK="
   docker compose -f compose.tests.yml run --quiet-pull --rm grants-ui-performance-tests &&
   docker compose -f compose.tests.yml down
