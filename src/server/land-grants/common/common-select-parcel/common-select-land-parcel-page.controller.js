c58af8ae5e768b6c915ceccecee78c25eda77cba
David Capilla <david.capilla@gmail.com>
2026-04-29 11:34:54 +0100

    TGC-1177 - Fix rounding issue with total parcel area on state

--- src/server/land-grants/common/common-select-parcel/common-select-land-parcel-page.controller.js
+++ src/server/land-grants/common/common-select-parcel/common-select-land-parcel-page.controller.js
@@ -177,7 +177,8 @@ export default class CommonSelectLandParcelPageController extends LandGrantsQues
     const landParcelMetadata = selectedParcelIds.map((id) => {
       const parcel = parcelMap.get(id)
       const rawArea = parcel?.area?.value
-      return { parcelId: id, areaHa: rawArea == null ? null : Number(rawArea) }
+      const areaHa = rawArea == null ? null : (Number(rawArea) * 10000) / 10000
+      return { parcelId: id, areaHa }
     })
 
     const totalHectaresForSelectedParcels =
