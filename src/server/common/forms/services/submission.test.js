8892c75db8979c99025037e90e9257994289286f
Stephen Powell <stephen.powell@esynergy.co.uk>
2026-05-01 14:47:01 +0100

    add multi field page and update dev tools

--- src/server/common/forms/services/submission.test.js
+++ src/server/common/forms/services/submission.test.js
@@ -16,14 +16,9 @@ describe('Validate submission answers', () => {
     monthYearField__year: 2026,
     selectField: 'selectFieldOption-A4',
     multilineTextField: 'This is a test',
-    applicantName: 'John Smith',
-    applicantEmail: 'john.smith@example.com',
-    applicantMobile: '07111 111111',
-    applicantBusinessAddress__addressLine1: '111 Test Street',
-    applicantBusinessAddress__addressLine2: null,
-    applicantBusinessAddress__town: 'Testville',
-    applicantBusinessAddress__county: null,
-    applicantBusinessAddress__postcode: 'TE5 7PO'
+    projectName: 'Test project',
+    projectDescription: 'Test description',
+    projectBudget: 50000
   }
 
   beforeAll(() => {
@@ -55,31 +50,21 @@ describe('Validate submission answers', () => {
 
   it('should allow missing optional fields without error', () => {
     const payloadWithoutOptionalField = { ...validPayload }
-    delete payloadWithoutOptionalField.applicantBusinessAddress__addressLine2
+    delete payloadWithoutOptionalField.projectDescription
 
     const result = validateSubmissionAnswers(payloadWithoutOptionalField, GRANT_CODE)
     expect(result.errors).toBeUndefined()
-    expect(result.value.applicantBusinessAddress__addressLine2).toBeUndefined()
-  })
-
-  it('should return an error if email fields are invalid', () => {
-    const invalidPayload = {
-      ...validPayload,
-      applicantEmail: 'not-an-email'
-    }
-    const result = validateSubmissionAnswers(invalidPayload, GRANT_CODE)
-    expect(result.errors).toBeDefined()
-    expect(result.errors[0].message).toContain('must match format "email"')
+    expect(result.value.projectDescription).toBeUndefined()
   })
 
   it('should return an error if missing required field', () => {
     const payloadWithoutRequiredField = { ...validPayload }
-    delete payloadWithoutRequiredField.applicantName
+    delete payloadWithoutRequiredField.projectName
 
     const result = validateSubmissionAnswers(payloadWithoutRequiredField, GRANT_CODE)
 
     expect(result.valid).toBe(false)
     expect(result.errors).toBeDefined()
-    expect(result.errors[0].message).toContain("must have required property 'applicantName'")
+    expect(result.errors[0].message).toContain("must have required property 'projectName'")
   })
 })
