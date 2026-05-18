d914e35fbe7c696b05e03eb90aaf1d5d3abf4762
David Barker <david.barker@equalexperts.com>
2026-05-18 09:39:11 +0100

    Refactored OIDC config

--- src/server/auth/get-oidc-config.js
+++ src/server/auth/get-oidc-config.js
@@ -1,10 +1,11 @@
 import Wreck from '@hapi/wreck'
 import { config } from '~/src/config/config.js'
 
-async function getOidcConfig(url = config.get('defraId.wellKnownUrl'), options = { json: true }) {
+async function getOidcConfig(url) {
+  const fetchUrl = url || config.get('defraId.wellKnownUrl')
   // Fetch the OpenID Connect configuration from the well-known endpoint
   // Contains the URLs for authorisation, sign out, token and public keys in JSON format
-  const { payload } = await Wreck.get(url, { ...options })
+  const { payload } = await Wreck.get(fetchUrl, { json: true })
 
   return payload
 }
