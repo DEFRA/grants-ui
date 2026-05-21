62b3e7023cf3dc1192bc21b4276a392a2ac241a0
Andrew Folga <andrew.folga@equalexperts.com>
2026-05-21 15:17:49 +0200

    Only use permissions given in the story.

--- src/server/common/helpers/permissions/permission-rules.js
+++ src/server/common/helpers/permissions/permission-rules.js
@@ -7,34 +7,5 @@ export const permissionRules = {
       amend: 'AMEND',
       submit: 'SUBMIT'
     }
-  },
-
-  csAgreements: {
-    permissionGroup: 'COUNTRYSIDE_STEWARDSHIP_AGREEMENTS',
-
-    permissions: {
-      view: 'VIEW',
-      amend: 'AMEND',
-      submit: 'SUBMIT'
-    }
-  },
-
-  landDetails: {
-    permissionGroup: 'LAND_DETAILS',
-
-    permissions: {
-      view: 'VIEW',
-      amend: 'AMEND'
-    }
-  },
-
-  businessDetails: {
-    permissionGroup: 'BUSINESS_DETAILS',
-
-    permissions: {
-      view: 'VIEW',
-      amend: 'AMEND',
-      submit: 'FULL_PERMISSION'
-    }
   }
 }
