1347866502e25d78768bed1cfe07dd056c53895e
David Barker <david.barker@equalexperts.com>
2026-05-28 09:06:08 +0100

    Moved environment variables inline with existing entra id platform setup

--- src/config/entra-id.js
+++ src/config/entra-id.js
@@ -5,31 +5,31 @@ const config = convict({
     doc: 'The Microsoft Entra ID well known URL.',
     format: String,
     default: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
-    env: 'ENTRA_ID_CONFIG_URL'
+    env: 'ENTRA_INTERNAL_CONFIG_URL'
   },
   clientId: {
     doc: 'The Microsoft Entra ID client ID.',
     format: String,
     default: 'default-client-id',
-    env: 'ENTRA_ID_CLIENT_ID'
+    env: 'ENTRA_INTERNAL_CLIENT_ID'
   },
   clientSecret: {
     doc: 'The Microsoft Entra ID client secret.',
     format: String,
     default: 'default-client-secret',
-    env: 'ENTRA_ID_CLIENT_SECRET'
+    env: 'ENTRA_INTERNAL_CLIENT_SECRET'
   },
   tenantId: {
     doc: 'The Microsoft Entra ID tenant ID.',
     format: String,
     default: 'common',
-    env: 'ENTRA_ID_TENANT_ID'
+    env: 'ENTRA_INTERNAL_TENANT_ID'
   },
   redirectUrl: {
     doc: 'The Microsoft Entra ID redirect URL.',
     format: String,
     default: 'https://default-redirect-url.com',
-    env: 'ENTRA_ID_REDIRECT_URL'
+    env: 'ENTRA_INTERNAL_REDIRECT_URL'
   }
 })
 
