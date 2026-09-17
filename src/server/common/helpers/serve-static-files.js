23642b108fc7a1b21be8f2981c7a71dd5950d052
David Capilla <david.capilla@equalexperts.com>
2026-09-17 13:36:10 +0100

    TGC-1679 - Fix maps css race condition issue (#1281)

--- src/server/common/helpers/serve-static-files.js
+++ src/server/common/helpers/serve-static-files.js
@@ -29,14 +29,6 @@ export const serveStaticFiles = {
         },
         {
           method: 'GET',
-          path: `${config.get('assetPath')}/stylesheets/interactive-map.css`,
-          handler: {
-            file: './stylesheets/interactive-map.css'
-          },
-          options
-        },
-        {
-          method: 'GET',
           path: '/assets/{path*}',
           handler: {
             directory: {
